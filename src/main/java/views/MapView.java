eb17161c77f2cfe9f5707f3d8d1e3de0b22fab20
Tahamii <94761016+Tahamii@users.noreply.github.com>
2022-04-25 16:23:54 +0430

    Update MapView.java

--- src/main/java/views/MapView.java
+++ src/main/java/views/MapView.java
@@ -17,49 +17,45 @@ public class MapView {
                         System.out.print(Color.RESET.getColor() + " ");
                     }
 
-                    if(i==6){
+                    if (i == 6) {
                         System.out.print(Color.RESET.getColor() + " ");
                     }
 
-                    for(int l = 0; l < 3; l++){
+                    for (int l = 0; l < 3; l++) {
 
-                        if(temp < 18){
+                        if (temp < 18) {
                             System.out.print(tiles.get(temp).hasRiver.get(5) + "/");
-                            for (int k = 2 * j + 5; k > 0; k--){
+                            for (int k = 2 * j + 5; k > 0; k--) {
                                 System.out.print(tiles.get(temp).getColor() + " ");
                             }
-                        }
-                        else{
-                            for (int k = 2 * j + 5; k > 0; k--){
+                        } else {
+                            for (int k = 2 * j + 5; k > 0; k--) {
                                 System.out.print(Color.RESET.getColor() + " ");
                             }
                         }
 
                         System.out.print(tiles.get(temp).hasRiver.get(1) + "\\");
 
-                        if(temp >= 5) {
+                        if (temp >= 5) {
                             for (int k = -2 * j + 9; k > 0; k--) {
-                                if(j==2){
+                                if (j == 2) {
                                     System.out.print(tiles.get(temp - 5).hasRiver.get(3) + "-");
-                                }
-                                else {
+                                } else {
                                     System.out.print(tiles.get(temp - 5).getColor() + " ");
                                 }
                             }
-                        }
-                        else{
+                        } else {
                             for (int k = -2 * j + 9; k > 0; k--) {
-                                if(j==2){
-                                    System.out.print(Color.RESET.getColor()+ "-");
-                                }
-                                else {
+                                if (j == 2) {
+                                    System.out.print(Color.RESET.getColor() + "-");
+                                } else {
                                     System.out.print(Color.RESET.getColor() + " ");
                                 }
                             }
                         }
 
-                        if((temp - 5 >= 0 && l==2 ) || temp > 17){
-                            System.out.print(tiles.get(temp - 5).hasRiver.get(2)+ "/" + Color.RESET.getColor());
+                        if ((temp - 5 >= 0 && l == 2) || temp > 17) {
+                            System.out.print(tiles.get(temp - 5).hasRiver.get(2) + "/" + Color.RESET.getColor());
                         }
 
                         temp += 2;
@@ -67,31 +63,28 @@ public class MapView {
                     temp -= 6;
                     System.out.println(Color.RESET.getColor());
                 }
-            }
-
-            else{
+            } else {
                 for (int j = 0; j < 3; j++) {
 
                     for (int k = j; k > 0; k--) {
                         System.out.print(Color.RESET.getColor() + " ");
                     }
 
-                    for(int l = 0; l < 3; l++) {
+                    for (int l = 0; l < 3; l++) {
                         System.out.print(tiles.get(temp).hasRiver.get(4) + "\\");
                         for (int k = -2 * j + 9; k > 0; k--) {
-                            if(j==2){
+                            if (j == 2) {
                                 System.out.print(tiles.get(temp).hasRiver.get(3) + "-");
-                            }
-                            else{
+                            } else {
                                 System.out.print(tiles.get(temp).getColor() + " ");
                             }
                         }
                         System.out.print(tiles.get(temp).hasRiver.get(2) + "/");
                         temp++;
-                        for (int k = 2 * j + 5; k > 0; k--){
+                        for (int k = 2 * j + 5; k > 0; k--) {
                             System.out.print(tiles.get(temp).getColor() + " ");
                         }
-                        if(l==2) {
+                        if (l == 2) {
                             System.out.print(tiles.get(temp).hasRiver.get(1) + "\\");
                         }
                         temp++;
