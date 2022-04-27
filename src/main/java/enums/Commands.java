eb2075d21feeacfdc72833c96ac269cf82d0a7f4
mahyazz <mahya.zarrabi@gmail.com>
2022-04-27 16:39:28 +0430

    Update Commands.java

--- src/main/java/enums/Commands.java
+++ src/main/java/enums/Commands.java
@@ -4,61 +4,57 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum Commands {
-    CREATEUSER1("\\s*user\\s+create\\s+(--username|-u)\\s+(?<username>\\S+)\\s+(--nickname|-n)\\s+(?<nickname>\\S+)\\s+(--password|-p)\\s+(?<password>\\S+)\\s*"),
-    CREATEUSER2("\\s*user\\s+create\\s+(--username|-u)\\s+(?<username>\\S+)\\s+(--password|-p)\\s+(?<password>\\S+)\\s+(--nickname|-n)\\s+(?<nickname>\\S+)\\s*"),
-    CREATEUSER3("\\s*user\\s+create\\s+(--password|-p)\\s+(?<password>\\S+)\\s+(--username|-u)\\s+(?<username>\\S+)\\s+(--nickname|-n)\\s+(?<nickname>\\S+)\\s*"),
-    CREATEUSER4("\\s*user\\s+create\\s+(--password|-p)\\s+(?<password>\\S+)\\s+(--nickname|-n)\\s+(?<nickname>\\S+)\\s+(--username|-u)\\s+(?<username>\\S+)\\s*"),
-    CREATEUSER5("\\s*user\\s+create\\s+(--nickname|-n)\\s+(?<nickname>\\S+)\\s+(--username|-u)\\s+(?<username>\\S+)\\s+(--password|-p)\\s+(?<password>\\S+)\\s*"),
-    CREATEUSER6("\\s*user\\s+create\\s+(--nickname|-n)\\s+(?<nickname>\\S+)\\s+(--password|-p)\\s+(?<password>\\S+)\\s+(--username|-u)\\s+(?<username>\\S+)\\s*"),
-    LOGIN1("\\s*user\\s+login\\s+(--username|-u)\\s+(?<username>\\S+)\\s+(--password|-p)\\s+(?<password>\\S+)\\s*"),
-    LOGIN2("\\s*user\\s+login\\s+(--password|-p)\\s+(?<password>\\S+)\\s+(--username|-u)\\s+(?<username>\\S+)\\s*"),
+    CREATEUSER1(
+            "user create (--username|-u) (?<username>\\S+) (--nickname|-n) (?<nickname>\\S+) (--password|-p) (?<password>\\S+)"),
+    CREATEUSER2(
+            "user create (--username|-u) (?<username>\\S+) (--password|-p) (?<password>\\S+) (--nickname|-n) (?<nickname>\\S+)"),
+    CREATEUSER3(
+            "user create (--password|-p) (?<password>\\S+) (--username|-u) (?<username>\\S+) (--nickname|-n) (?<nickname>\\S+)"),
+    CREATEUSER4(
+            "user create (--password|-p) (?<password>\\S+) (--nickname|-n) (?<nickname>\\S+) (--username|-u) (?<username>\\S+)"),
+    CREATEUSER5(
+            "user create (--nickname|-n) (?<nickname>\\S+) (--username|-u) (?<username>\\S+) (--password|-p) (?<password>\\S+)"),
+    CREATEUSER6(
+            "user create (--nickname|-n) (?<nickname>\\S+) (--password|-p) (?<password>\\S+) (--username|-u) (?<username>\\S+)"),
+    LOGIN1("user login (--username|-u) (?<username>\\S+) (--password|-p) (?<password>\\S+)"),
+    LOGIN2("user login (--password|-p) (?<password>\\S+) (--username|-u) (?<username>\\S+)"),
 
+    MENUENTER("menu enter (?<menuname>\\S+)"),
+    MENUEXIT("menu exit"),
+    LOGOUT("user logout"),
+    SHOWMENU("menu show-current"),
 
+    PLAYGAME("play game --player1 (?<username1>\\S+) --player2(?<username2>\\S+)"),
 
+    NICKNAMECHANGE("profile change --nickname (?<newnickname>\\S+)"),
+    CHANGEPASSWORD1(
+            "profile change --password --current (?<currentpassword>\\S+) --new (?<newpassword>\\S+)"),
+    CHANGEPASSWORD2(
+            "profile change --new\\s(?<newpassword>\\S+) --password --current\\s(?<currentpassword>\\S+)"),
 
-    MENUENTER("\\s*menu\\s+enter\\s+(?<menuname>\\S+)\\s*"),
-    MENUEXIT("\\s*menu\\s+exit"),
-    LOGOUT("\\s*user\\s+logout"),
-    SHOWMENU("\\s*menu\\s+show-current\\s*"),
-
-
-
-    PLAYGAME("\\s*play\\s+game\\s+--player1\\s+(?<username1>\\S+)\\s+--player2(?<username2>\\S+)\\s*"),
-
-
-    NICKNAMECHANGE("\\s*profile\\s+change\\s+--nickname\\s+(?<newnickname>\\S+)"),
-    CHANGEPASSWORD1("\\s*profile\\s+change\\s+--password\\s+--current\\s+(?<currentpassword>\\S+)\\s+--new\\s+(?<newpassword>\\S+)\\s*"),
-    CHANGEPASSWORD2("\\s*profile\\s+change\\s+--new\\s(?<newpassword>\\S+)\\s+--password\\s+--current\\s(?<currentpassword>\\S+)\\s*"),
-
-
-
-    //game enums
-    INFORESEARCH("\\s*info\\s+research\\s*"),
-    INFOUNIT("\\s*info\\s+unit\\s*"),
-    INFOCITY("\\s*info\\s+city\\s*"),
-    INFODIPLOMACY("\\s*info\\s+diplomacy\\s*"),
-    INFOVICTORY("\\s*info\\s+victory\\s*"),
-    INFODEMOGRAPHICS("\\s*info\\s+demographics\\s*"),
-    INFONOTIFICATIONS("\\s*info\\s+notification\\s*"),
-    INFOMILITARY("\\s*info\\s+military\\s*"),
-    INFOECONOMIC("\\s*info\\s+economic\\s*"),
-    INFODIPLOMATIC("\\s*info\\s+diplomacy\\s*"),
-    INFODEALS("\\s*info\\s+deals\\s*");
-
-
-
-
-
-
+    // game enums
+    INFORESEARCH("info research"),
+    INFOUNIT("info unit"),
+    INFOCITY("info city"),
+    INFODIPLOMACY("info diplomacy"),
+    INFOVICTORY("info victory"),
+    INFODEMOGRAPHICS("info demographics"),
+    INFONOTIFICATIONS("info notification"),
+    INFOMILITARY("info military"),
+    INFOECONOMIC("info economic"),
+    INFODIPLOMATIC("info diplomacy"),
+    INFODEALS("info deals");
 
     private String regex;
-    Commands (String regex){
+
+    Commands(String regex) {
         this.regex = regex;
     }
 
-    public static Matcher getMatcher(String input, Commands command){
-        Matcher matcher = Pattern.compile(command.regex, Pattern.CASE_INSENSITIVE).matcher(input);
-        if(matcher.matches()) return matcher;
-        return null;
+    public static Matcher getMatcher(String input, Commands command) {
+        String regex = "\\s*" + command.regex.replace(" ", " ") + "\\s*";
+        Matcher matcher = Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(input);
+        return matcher.matches() ? matcher : null;
+
     }
 }
