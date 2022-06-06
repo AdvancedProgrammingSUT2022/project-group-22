3a99a1790351ba7cb6f465eebc1cbe08637d2530
mahyazz <mahya.zarrabi@gmail.com>
2022-06-06 13:17:02 +0430

    Add basic chatroom draft

--- src/main/java/civilization/App.java
+++ src/main/java/civilization/App.java
@@ -1,7 +1,7 @@
 package civilization;
 
 import civilization.controllers.RegisterMenuController;
-import civilization.views.FirstPage;
+import civilization.views.*;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
